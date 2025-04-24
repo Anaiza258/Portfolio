b46dd060e7a1f1840d9c088ae6fbca057a780c51
Anaiza258 <anaiza7869@gmail.com>
2025-04-24 12:26:40 +0500

    new

--- csv_to_llm.py
+++ csv_to_llm.py
@@ -489,8 +489,8 @@ def submit_contact():
 
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 8000))  # Koyeb provides PORT dynamically
-    # app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))  # Koyeb provides PORT dynamically
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
 
 
